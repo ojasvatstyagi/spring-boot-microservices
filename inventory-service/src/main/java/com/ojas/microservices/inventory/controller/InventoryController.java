0c782e86cdc30ce65e5ef15ec2ea98507cbd7c4b
Ojas vats tyagi <ojastyagi753@gmail.com>
2025-06-30 15:45:00 +0530

    Add inventory service server code

--- inventory-service/src/main/java/com/ojas/microservices/inventory/controller/InventoryController.java
+++ inventory-service/src/main/java/com/ojas/microservices/inventory/controller/InventoryController.java
@@ -1,4 +1,20 @@
 package com.ojas.microservices.inventory.controller;
 
+import com.ojas.microservices.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
 }
--- inventory-service/src/main/java/com/ojas/microservices/inventory/service/InventoryService.java
+++ inventory-service/src/main/java/com/ojas/microservices/inventory/service/InventoryService.java
@@ -1,4 +1,18 @@
 package com.ojas.microservices.inventory.service;
 
+import com.ojas.microservices.inventory.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode, Integer quantity) {
+        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+    }
 }
