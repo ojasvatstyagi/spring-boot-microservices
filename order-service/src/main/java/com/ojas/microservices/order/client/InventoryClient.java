e70402d97c13068878733e2652d05c311e824498
Ojas vats tyagi <ojastyagi753@gmail.com>
2025-07-08 12:30:00 +0530

    used rest client for inter process communication removed OpenFeign

--- order-service/src/main/java/com/ojas/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/ojas/microservices/order/client/InventoryClient.java
@@ -1,14 +1,12 @@
 package com.ojas.microservices.order.client;
 
 import com.ojas.microservices.order.dto.InventoryResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @GetMapping("/api/inventory")
+    @GetExchange("/api/inventory")
     InventoryResponse isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
 }
