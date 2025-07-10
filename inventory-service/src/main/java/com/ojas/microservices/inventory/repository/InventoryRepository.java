8201496dd06eded43f93f610a1c65dd5a63504b1
Ojas vats tyagi <ojastyagi753@gmail.com>
2025-07-10 23:22:43 +0530

    Add Inventory service project structure

---
+++ inventory-service/src/main/java/com/ojas/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,4 @@
+package com.ojas.microservices.inventory.repository;
+
+public interface InventoryRepository {
+}
---
+++ inventory-service/src/main/java/com/ojas/microservices/inventory/service/InventoryService.java
@@ -0,0 +1,4 @@
+package com.ojas.microservices.inventory.service;
+
+public class InventoryService {
+}
