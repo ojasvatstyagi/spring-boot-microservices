7a1bcdb59295900a266fc44c20ffda9ef90e0a92
Ojas vats tyagi <ojastyagi753@gmail.com>
2025-07-11 16:15:19 +0530

    Update Order service code for more endpoints

--- order-service/src/main/java/com/ojas/microservices/order/dto/OrderResponse.java
+++ order-service/src/main/java/com/ojas/microservices/order/dto/OrderResponse.java
@@ -1,4 +1,18 @@
 package com.ojas.microservices.order.dto;
 
-public record OrderResponse() {
+import com.ojas.microservices.order.model.Order;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+    public static OrderResponse fromOrder(Order order) {
+        return new OrderResponse(
+                order.getId(),
+                order.getOrderNumber(),
+                order.getSkuCode(),
+                order.getPrice(),
+                order.getQuantity()
+        );
+    }
 }
+
--- order-service/src/main/java/com/ojas/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/ojas/microservices/order/service/OrderService.java
@@ -1,12 +1,15 @@
 package com.ojas.microservices.order.service;
 
 import com.ojas.microservices.order.dto.OrderRequest;
+import com.ojas.microservices.order.dto.OrderResponse;
+import com.ojas.microservices.order.exception.OrderNotFoundException;
 import com.ojas.microservices.order.model.Order;
 import com.ojas.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -16,10 +19,12 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
         var order = mapToOrder(orderRequest);
         orderRepository.save(order);
+        return OrderResponse.fromOrder(order);
     }
+
 
     private static Order mapToOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -28,5 +33,18 @@ public class OrderService {
         order.setQuantity(orderRequest.quantity());
         order.setSkuCode(orderRequest.skuCode());
         return order;
+    }
+
+    public List<OrderResponse> getAllOrders() {
+        return orderRepository.findAll()
+                .stream()
+                .map(OrderResponse::fromOrder)
+                .toList();
+    }
+
+    public OrderResponse getOrderById(Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new OrderNotFoundException("Order not found with id: " + id));
+        return OrderResponse.fromOrder(order);
     }
 }
